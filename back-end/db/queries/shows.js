4946cf3b727aa1b6e083d0a96090235b0fe77a08
vonbarown <vonielbrown@pursuit.org>
2020-02-08 13:17:36 -0500

    get shows by id and genre_id

--- back-end/db/queries/shows.js
+++ back-end/db/queries/shows.js
@@ -4,7 +4,29 @@ const db = require('../db')
 const getAllShows = async () => db.any("SELECT * FROM shows")
 
 
+const getShowsById = async (id) => db.any("SELECT * from shows WHERE id = $1", [id])
+
+const addNewShow = async (showObj) => {
+    console.log(showObj.title);
+
+    const newShowQStr = `INSERT INTO shows (title, img_url,user_id,genre_id) 
+VALUES($/title/,$/img_url/,$/user_id/,$/genre_id/) RETURNING *`
+
+    return db.one(newShowQStr, {
+        title: showObj.title,
+        img_url: showObj.img_url,
+        user_id: showObj.user_id,
+        genre_id: showObj.genre_id
+    })
+}
+
+
+const getShowsByGenreId = async (genreId) => db.any("SELECT * from shows WHERE genre_id = $1", [genreId])
+
 
 module.exports = {
-    getAllShows
+    getAllShows,
+    getShowsById,
+    getShowsByGenreId,
+    addNewShow
 }
--- back-end/routes/shows.js
+++ back-end/routes/shows.js
@@ -21,4 +21,63 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+router.get('/:id', async (req, res, next) => {
+    try {
+        let byId = await queries.getShowsById(req.params.id)
+
+        res.json({
+            shows: byId,
+            message: 'show retrieved',
+            error: false
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            shows: null,
+            message: 'you took a wrong turn',
+            error: true
+        })
+    }
+})
+
+router.get('/genre/:genre_id', async (req, res, next) => {
+    try {
+        let byGenreId = await queries.getShowsByGenreId(req.params.genre_id)
+
+        res.json({
+            shows: byGenreId,
+            message: 'show retrieved',
+            error: false
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            shows: null,
+            message: 'you took a wrong turn',
+            error: true
+        })
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    console.log(req.body);
+    try {
+        let newShow = await queries.addNewShow(req.body)
+
+        res.json({
+            shows: newShow,
+            message: 'new series added',
+            error: false
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            shows: null,
+            message: 'series could not be added',
+            error: true
+        })
+    }
+})
+
+
 module.exports = router
