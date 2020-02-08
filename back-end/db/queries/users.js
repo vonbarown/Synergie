f9467cbc3d554758c8a853fef92467183656e225
vonbarown <vonielbrown@pursuit.org>
2020-02-08 11:30:47 -0500

    finished mvp of the users routes

--- back-end/db/queries/users.js
+++ back-end/db/queries/users.js
@@ -5,6 +5,24 @@ const getAllUsers = async () => {
     return db.any("SELECT * FROM users")
 }
 
+const getUsersById = async (id) => {
+    return db.any("SELECT * from users WHERE id = $1", [id])
+}
+
+const addNewUser = async (userObj) => {
+    console.log('userObj', userObj);
+
+    const newUserQStr = `INSERT INTO users (username, avatar_url) 
+VALUES($/username/,$/avatar_url/) RETURNING *`
+
+    return db.one(newUserQStr, {
+        username: userObj.username,
+        avatar_url: userObj.avatar_url
+    })
+}
+
 module.exports = {
-    getAllUsers
+    getAllUsers,
+    getUsersById,
+    addNewUser
 }
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const queries = require('../db/queries/users')
 
 /* GET users listing. */
-router.get('/', async function (req, res, next) {
+router.get('/', async (req, res, next) => {
   try {
     let users = await queries.getAllUsers()
 
@@ -20,5 +20,43 @@ router.get('/', async function (req, res, next) {
     })
   }
 });
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    let byId = await queries.getUsersById(req.params.id)
+
+    res.json({
+      users: byId,
+      message: 'all users retrieved',
+      error: false
+    })
+  } catch (error) {
+    res.status(500).json({
+      users: null,
+      message: 'you took a wrong turn',
+      error: true
+    })
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  console.log(req.body);
+  try {
+    let newUser = await queries.addNewUser(req.body)
+
+    res.json({
+      users: newUser,
+      message: 'all users retrieved',
+      error: false
+    })
+  } catch (error) {
+    res.status(500).json({
+      users: null,
+      message: 'user could not be added',
+      error: true
+    })
+  }
+})
+
 
 module.exports = router;
