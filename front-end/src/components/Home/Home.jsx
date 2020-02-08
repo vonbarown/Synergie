5f2e88bd84daa3b4644128a5872322bd719edac9
vonbarown <vonielbrown@pursuit.org>
2020-02-08 17:05:37 -0500

    mvp homepage

--- front-end/src/components/Home/Home.jsx
+++ front-end/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import './home.css'
 
 export const Home = () => {
     return (
         <div className='home'>
-            homer
+            <div className='home-logo'>Home</div>
+            <h1>Welcome to TV Watchlist App</h1>
         </div>
     )
 }
