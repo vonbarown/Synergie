8454728bad145b41f1eb72ade052aa750466c7a1
vonbarown <vonielbrown@pursuit.org>
2020-02-09 17:28:07 -0500

    starting work on the addshow form

--- front-end/src/components/AddShowForm/AddShowForm.jsx
+++ front-end/src/components/AddShowForm/AddShowForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './AddShow.css'
 class AddShowForm extends React.Component {
     sate = {
         img_url: '',
@@ -8,10 +8,24 @@ class AddShowForm extends React.Component {
 
     render() {
         return (
-            <div className='add-show-form'>
-                <form>
-                    <input type="text" value='url' />
-                </form>
+            <div className='add-show-form-page'>
+                <div className='add-show-form'>
+                    <h1>Add Show</h1>
+                    <h2>Form</h2>
+                    <form>
+                        <div className='url'>
+                            <input type="text" placeholder='url' />
+                        </div>
+                        <div className='name'>
+                            <input type="text" placeholder='Name' />
+                        </div>
+                        <div className='genre'>
+                            <select>
+                                <option>---Select A Genre---</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
