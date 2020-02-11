b8b2ca6836d04c59271832fe5ec21dfd151f6a3e
vonbarown <vonielbrown@pursuit.org>
2020-02-10 19:15:07 -0500

    fixed how the  object is implemented and is then populated

--- front-end/src/store/reducers/showsReducer.js
+++ front-end/src/store/reducers/showsReducer.js
@@ -12,26 +12,21 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case LOAD_SHOWS:
             stateCopy.shows = action.payload
-            const dupes = {};
-            const filteredShows = [];
             let watchList = {}
 
             for (let i = 0; i < stateCopy.shows.length; i++) {
                 let key = stateCopy.shows[i].title
                 if (!watchList[key]) {
                     watchList[key] = []
-                } else {
-                    watchList[key].push(stateCopy.shows[i].user_id)
-                }
+                    watchList[key].push(stateCopy.shows[i].username)
 
-                // if (!dupes[key]) {
-                //     filteredShows.push(stateCopy.shows[i])
-                // }
-                dupes[key] = true
+                } else {
+                    watchList[key].push(stateCopy.shows[i].username)
+                }
             }
             console.log('watchList', watchList);
 
-            stateCopy.shows = filteredShows
+            stateCopy.shows = action.payload
 
             break
         case LOAD_USER_SHOWS:
