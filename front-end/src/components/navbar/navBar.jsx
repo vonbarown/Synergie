8f0f43104631dcac845358dbc0c5e9f2f364c7a0
vonbarown <vonielbrown@pursuit.org>
2020-02-12 17:04:46 -0500

    adding authentication to the navbar

--- front-end/src/components/navbar/navBar.jsx
+++ front-end/src/components/navbar/navBar.jsx
@@ -2,26 +2,57 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './navbar.css'
 
-export const Navbar = ({ logoutUser }) => {
-    return (
-        <div className='navbar'>
+export const Navbar = ({ logoutUser, isUserLoggedIn }) => {
+    // return (
+    //     <div className='navbar'>
 
-            <Link to='/'>
-                <div className='logo'>
-                    <h2>Synergie</h2>
-                </div>
-            </Link>
+    //         <Link to='/'>
+    //             <div className='logo'>
+    //                 <h2>Synergie</h2>
+    //             </div>
+    //         </Link>
 
 
-            <div className='links'>
-                <Link to='/users'>Users</Link>{" "}
+    //         <div className='links'>
+    //             <Link to='/users'>Users</Link>{" "}
+    //             <Link to='/shows'>Shows</Link>{' '}
+    //             <Link to='/about'>About</Link>{' '}
+    //             <Link to='/addShow'>Add Show</Link>{' '}
+    //             <Link to='/login'>Login</Link>{' '}
+    //             <Link to='/signup'>Sign Up</Link>
+    //             <button onClick={logoutUser}>log-out</button>
+    //         </div>
+    //     </div>
+    // )
+    if (isUserLoggedIn) {
+        return (
+            <nav className='logged-in'>
+                <Link to="/">
+                    <div className='logo'>
+                        <h2>Synergie</h2>
+                    </div>
+                </Link>{" "}
+                <Link to='/users'>Users</Link>
                 <Link to='/shows'>Shows</Link>{' '}
-                <Link to='/about'>About</Link>{' '}
                 <Link to='/addShow'>Add Show</Link>{' '}
-                <Link to='/login'>Login</Link>{' '}
-                <Link to='/signup'>Sign Up</Link>
-                <button onClick={logoutUser}>log-out</button>
-            </div>
-        </div>
-    )
+                <button className='log-out' onClick={logoutUser}>log-out</button>
+            </nav>
+
+        )
+    } else {
+        return (
+            <nav className='not-logged-in'>
+                <Link to="/">
+                    <Link to="/">
+                        <div className='logo'>
+                            <h2>Synergie</h2>
+                        </div>
+                    </Link>{" "}
+                </Link>{" "}
+                <Link to='/about'>About</Link>{' '}
+                <Link to='login'>Log-In</Link>{' '}
+                <Link to='/signup'>Sign-Up</Link>{' '}
+            </nav>
+        )
+    }
 }
