d0fb4cba3dfb119062383aab42f80596ac5e0c47
vonbarown <vonielbrown@pursuit.org>
2020-02-11 23:44:29 -0500

    starting front end authentication

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,23 +9,36 @@ import showsContainer from './containers/showsContainer';
 import { About } from './components/About/About';
 import AddShowForm from './components/AddShowForm/AddShowForm';
 import ShowInfoPage from './components/ShowInfoPage/ShowInfoPage';
+import AuthContainer from './containers/authContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
+class App extends React.Component {
 
-      <Switch>
-        <Route exact path='/users' component={UsersContainer} />
-        <Route exact path='/users/:id' component={UserPage} />
-        <Route exact path='/addShow' component={AddShowForm} />
-        <Route exact path='/shows' component={showsContainer} />
-        <Route exact path='/shows/:id/user/:userId' component={ShowInfoPage} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/' component={Home} />
-      </Switch>
-    </div>
-  );
+
+
+  renderAuthContainer = (routeProps) => <AuthContainer setUser={this.setUser}{...routeProps} isUserLoggedIn={this.state.isUserLoggedIn} />
+
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+
+
+
+        <Switch>
+          <Route path='/login' render={this.renderAuthContainer} />
+          <Route path='/signup' render={this.renderAuthContainer} />
+          <Route exact path='/users' component={UsersContainer} />
+          <Route exact path='/users/:id' component={UserPage} />
+          <Route exact path='/addShow' component={AddShowForm} />
+          <Route exact path='/shows' component={showsContainer} />
+          <Route exact path='/shows/:id/user/:userId' component={ShowInfoPage} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ front-end/src/containers/authContainer.jsx
@@ -0,0 +1,94 @@
+import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import LoginForm from '../components/AuthComponents/LoginForm'
+import SignupForm from '../components/AuthComponents/SignUpForm'
+import axios from 'axios'
+
+class AuthContainer extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+    async componentDidMount() {
+        try {
+            const { data } = await axios.get('/auth/isUserLoggedIn')
+            this.props.setUser(data.payload)
+        } catch (err) {
+            console.log('ERROR', err)
+        }
+    }
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value })
+
+    signupUser = async (user) => {
+        try {
+            const { data } = await axios.post('/auth/signup', this.state)
+
+            this.props.setUser(data.payload)
+
+        } catch (error) {
+            console.log(error);
+
+        }
+
+    }
+
+    loginUser = async () => {
+        try {
+            const { data } = await axios.post('/auth/login', this.state)
+
+            this.props.setUser(data.payload)
+            this.props.history.push('/users')
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    renderSignupForm = () => {
+        const { username, password } = this.state
+        return (
+            <SignupForm
+                handleChange={this.handleChange}
+                username={username}
+                password={password}
+                signupUser={this.signupUser}
+            />
+        )
+    }
+
+    renderLoginForm = () => {
+        const { username, password } = this.state
+        return (
+            <LoginForm
+                handleChange={this.handleChange}
+                username={username}
+                password={password}
+                loginUser={this.loginUser}
+            />
+        )
+    }
+
+    checkUserLoggedIn = () => {
+
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.isUserLoggedIn
+                        ? <Redirect to='/profile' />
+                        : (
+                            <Switch>
+                                <Route path='/login' component={this.renderLoginForm} />
+                                <Route path='/signup' render={this.renderSignupForm} />
+                            </Switch>
+                        )
+                }
+            </div>
+        )
+    }
+}
+
+export default AuthContainer
