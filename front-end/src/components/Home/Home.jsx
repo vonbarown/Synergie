b9af90d5fae9c41970de7e8833606d96b5be8ed3
vonbarown <vonielbrown@pursuit.org>
2020-02-12 20:21:08 -0500

    adding logo to app

--- front-end/src/components/Home/Home.jsx
+++ front-end/src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './home.css'
+import logo from '../../logo/07f3beb4-475b-4f38-b462-0d2dae119fd2_200x200.png'
 
 export const Home = () => {
     return (
         <div className='home'>
-            <div className='home-logo'>Home</div>
-            <h1>Welcome to TV Watchlist App</h1>
+            <img className='home-logo' src={logo} alt="" />
+            <h1 className='banner'>Welcome to Synergie</h1>
         </div>
     )
 }
