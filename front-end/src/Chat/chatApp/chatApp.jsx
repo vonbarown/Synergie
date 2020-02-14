4fb2021d5bf790a2e20b2988e6c81dbddeef609d
vonbarown <vonielbrown@pursuit.org>
2020-02-13 19:40:17 -0500

    setting up the environment for the chat app

--- front-end/src/Chat/chatApp/chatApp.jsx
+++ front-end/src/Chat/chatApp/chatApp.jsx
@@ -3,8 +3,8 @@ import { RootComponent } from '../RootComponent'
 import PubNub from 'pubnub';
 import { PubNubProvider } from 'pubnub-react';
 
-const pubNubConfig = require('./pubnub.config.json');
-const pubNubClient = new PubNub(pubNubConfig.keySet);
+// const pubNubConfig = require('./pubnub.config.json');
+const pubNubClient = new PubNub(process.env.keySet);
 
 export const ChatApp = () => {
     return (
