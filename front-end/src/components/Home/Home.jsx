fc0ce0cadd1c7f713a657ee2562bf93d59a388f3
vonbarown <vonielbrown@pursuit.org>
2020-02-20 22:25:35 -0500

    redesigning homepage

--- front-end/src/components/Home/Home.jsx
+++ front-end/src/components/Home/Home.jsx
@@ -5,8 +5,10 @@ import logo from '../../logo/07f3beb4-475b-4f38-b462-0d2dae119fd2_200x200.png'
 export const Home = () => {
     return (
         <div className='home'>
-            <img className='home-logo' src={logo} alt="" />
-            <h1 className='banner'>Welcome to Synergie</h1>
+            <div className='home-metadata'>
+                <img className='home-logo' src={logo} alt="" />
+                <h1 className='banner'>Welcome to Synergie</h1>
+            </div>
         </div>
     )
 }
