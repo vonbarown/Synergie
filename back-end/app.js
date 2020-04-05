441b8e0d67f4355dff3c370df73bab88fb64ade9
vonbarown <vonielbrown@pursuit.org>
2020-04-05 15:34:34 -0400

    heroku version

--- back-end/app.js
+++ back-end/app.js
@@ -27,7 +27,7 @@ app.use(
   session({
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: true,
   })
 );
 
@@ -44,7 +44,7 @@ app.use("/api/network", networkRouter);
 // app.use('/api/message', messageRouter)
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../front-end/build/index.html"));
+  res.sendFile(path.join(__dirname, "../front-end/build/index.html"));
 });
 
 module.exports = app;
