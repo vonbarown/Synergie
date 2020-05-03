c3ae1ba8051997dace5f4c7eafdeb5fe04bf7b81
vonbarown <vonielbrown@pursuit.org>
2020-05-03 11:51:25 -0400

    updating the shows page styling

--- front-end/src/components/Shows/Shows.jsx
+++ front-end/src/components/Shows/Shows.jsx
@@ -1,111 +1,117 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import './showPage.css'
-import { Link } from 'react-router-dom'
-import { animateScroll as scroll } from 'react-scroll'
+import React from "react";
+import { connect } from "react-redux";
+import "./showPage.css";
+import { Link } from "react-router-dom";
+import { animateScroll as scroll } from "react-scroll";
 // import axios from 'axios'
-import { scrolling } from '../../store/actions/userActions'
-import { Watchers } from './watchers'
+import { scrolling } from "../../store/actions/userActions";
+import { Watchers } from "./watchers";
 class Shows extends React.Component {
-    state = {
-        prevScrollPos: window.pageYOffset,
-        visible: false
-    }
+  state = {
+    prevScrollPos: window.pageYOffset,
+    visible: false,
+  };
 
-    componentDidMount() {
-        window.addEventListener("scroll", this.handleScroll);
-    }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
+  // toggles the visibility of the scroll to top bottom
+  handleScroll = () => {
+    const { prevScrollPos } = this.state;
 
-    // toggles the visibility of the scroll to top bottom
-    handleScroll = () => {
-        const { prevScrollPos } = this.state;
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollPos < currentScrollPos;
 
-        const currentScrollPos = window.pageYOffset;
-        const visible = prevScrollPos < currentScrollPos;
+    this.setState({
+      prevScrollPos: currentScrollPos,
+      visible,
+    });
+    // this.props.scrolling(window.pageYOffset)
+  };
 
-        this.setState({
-            prevScrollPos: currentScrollPos,
-            visible
-        });
-        // this.props.scrolling(window.pageYOffset)
-    };
+  render() {
+    const { visible } = this.state;
+    const { user, startWatching } = this.props;
 
+    return (
+      <div className="user-page" onScroll={this.handleScroll}>
+        <div className="container">
+          <h2 className="page-title">All Shows</h2>
+          {Object.values(this.props.watchList).map((show) => {
+            return (
+              <Link to={`/shows/${show.id}/user/${show.user_id}`} key={show.id}>
+                {/* creating link  to show page to see comments on show*/}
+                <div className="movie" key={show.title}>
+                  <img
+                    className="show-img"
+                    src={show.img_url}
+                    alt={show.title}
+                  />
+                  <div className="show-info">
+                    <div className="show-meta-data">
+                      <p className="title">{show.title}</p>
+                      <p className="genre">{show.genre}</p>
+                    </div>
 
+                    {/* all the watchers for show*/}
+                    <Watchers watchers={show.watchers} />
 
-
-    render() {
-        const { visible } = this.state
-        const { user, startWatching } = this.props
-
-        return (
-            <div className='user-page' onScroll={this.handleScroll}>
-                <div className='container'>
-                    <h2 className='page-title'>All Shows</h2>
                     {
-                        Object.values(this.props.watchList).map(show => {
-
-                            return (
-                                <Link to={`/shows/${show.id}/user/${show.user_id}`} key={show.id}>
-                                    {/* creating link  to show page to see comments on show*/}
-                                    <div className='movie' key={show.title}>
-                                        <img className='show-img' src={show.img_url} alt={show.title} />
-                                        <div className='show-info'>
-                                            <div>
-                                                <p className='title'>{show.title}</p>
-                                                <p className='genre'>{show.genre}</p>
-                                            </div>
-
-                                            {/* all the watchers for show*/}
-                                            <Watchers watchers={show.watchers} />
-
-                                            {
-                                                /*
+                      /*
                                                 Allowing user to start watching show without having too add
                                                 with form.Only shows if user is not watching show
                                                 */
-                                                !show.watchers.find(el => el.watchers_id === user.id)
-                                                    ? <button className='form-button ' id='start-watching'
-                                                        value={show.id} onClick={startWatching}
-                                                    >
-                                                        Start watching
-                                                </button>
-                                                    : null
-                                            }
-                                        </div>
-                                    </div>
-                                </Link>
-                            )
-                        })
+                      !show.watchers.find(
+                        (el) => el.watchers_id === user.id
+                      ) ? (
+                        <button
+                          className="form-button "
+                          id="start-watching"
+                          value={show.id}
+                          onClick={startWatching}
+                        >
+                          Start watching
+                        </button>
+                      ) : null
                     }
+                  </div>
                 </div>
-                {
-                    // checking  if the button should be visible based on scroll position
-                    visible
-                        ? <button className='scroll' onClick={() => scroll.scrollToTop()}>Scroll To Top</button>
-                        : null
-
-                }
-            </div>
-        )
-    }
+              </Link>
+            );
+          })}
+        </div>
+        {
+          // checking  if the button should be visible based on scroll position
+          visible ? (
+            <button className="scroll" onClick={() => scroll.scrollToTop()}>
+              Scroll To Top
+            </button>
+          ) : null
+        }
+      </div>
+    );
+  }
 }
-
 
 // redux functions connecting component to redux state and and actions
-const mapStateToProps = ({ showsReducer: { shows, showObj }, usersReducer: { loggedUser: { user } } }) => {
-    return {
-        shows: shows,
-        watchList: showObj,
-        user: user,
-    }
-}
+const mapStateToProps = ({
+  showsReducer: { shows, showObj },
+  usersReducer: {
+    loggedUser: { user },
+  },
+}) => {
+  return {
+    shows: shows,
+    watchList: showObj,
+    user: user,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        scrolling: data => dispatch(scrolling(data))
-    }
-}
+  return {
+    scrolling: (data) => dispatch(scrolling(data)),
+  };
+};
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Shows)
+export default connect(mapStateToProps, mapDispatchToProps)(Shows);
--- front-end/src/components/Shows/watchers.jsx
+++ front-end/src/components/Shows/watchers.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Watchers = ({ watchers }) => {
-    return (
-        <div className='watchers-on-wall'>
-            <h3>Being Watched by:</h3>
-            <div className='show-watchers'>
-                {
-                    watchers.map(watcher => {
-                        return (
-                            <div className={`watcher-${watcher.watchers_id}`} key={watcher.username}>
-                                <Link to={`/users/${watcher.watchers_id}`}>
-                                    <img className='watcher-img'
-                                        src={watcher.avatar_url}
-                                        alt={watcher.username}
-                                    />
-                                    {watcher.username}
-                                </Link>{' '}
-                            </div>
-                        )
-                    })
-                }
+  return (
+    <div className="watchers-on-wall">
+      <h3>Being Watched by:</h3>
+      <div className="show-watchers">
+        {watchers.map((watcher) => {
+          return (
+            <div
+              className={`watcher-${watcher.watchers_id} `}
+              key={watcher.username}
+            >
+              <Link to={`/users/${watcher.watchers_id}`} className="watcher">
+                <img
+                  className="watcher-img"
+                  src={watcher.avatar_url}
+                  alt={watcher.username}
+                />
+                <p className="name">{watcher.username}</p>
+              </Link>{" "}
             </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
