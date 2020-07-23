512f1192798677cacf80f89ab84aba9db0ffd007
vonbarown <vonielbrown@pursuit.org>
2020-07-22 22:23:12 -0400

    disable search card button on submit

--- front-end/src/components/OMDB/searchCard/back.jsx
+++ front-end/src/components/OMDB/searchCard/back.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./card.scss";
 
 const Back = (props) => {
+  const [submit, setSubmit] = useState(false);
   const searchObj = {
     title: props.result.Title,
     img_url: props.result.Poster,
@@ -16,7 +17,19 @@ const Back = (props) => {
       <p>{props.result.Actors}</p>
       <p>Awards</p>
       <p>{props.result.Awards}</p>
-      <button onClick={() => props.addShow(searchObj)}>Submit</button>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setSubmit(true);
+        }}
+      >
+        <button
+          className={submit ? "submitted" : "un-submitted"}
+          onClick={() => props.addShow(searchObj)}
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
