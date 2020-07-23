00718e4e0e0ea7a4db30f3cda8666c60700b578e
vonbarown <vonielbrown@pursuit.org>
2020-07-22 22:14:03 -0400

    fromat changes

--- back-end/db/queries/shows.js
+++ back-end/db/queries/shows.js
@@ -1,8 +1,7 @@
-const db = require('../db')
-
+const db = require("../db");
 
 const getAllShows = async () => {
-    const queryStr = `SELECT
+  const queryStr = `SELECT
                         shows.id,title,img_url,showWatchers.user_id 
                         AS watchers_id, users.username,
                         genres.genre_name,users.avatar_url, shows.user_id
@@ -12,64 +11,62 @@ const getAllShows = async () => {
                         INNER JOIN users ON showWatchers.user_id = users.id
                         INNER JOIN genres ON genres.id = shows.genre_id
                         ORDER BY shows.id DESC
-                        `
+                        `;
 
-    return await db.any(queryStr)
-}
-
+  return await db.any(queryStr);
+};
 
 const getShowsById = async (id) => {
-    const queryStr = `SELECT 
+  const queryStr = `SELECT 
                         shows.id,shows.title,shows.img_url,
                         genres.genre_name, users.username
                         FROM shows 
                         INNER JOIN genres ON genre_id = genres.id 
                         INNER JOIN users ON user_id = users.id
-                        WHERE shows.id = $1`
+                        WHERE shows.id = $1`;
 
-    return await db.any(queryStr, [id])
-}
+  return await db.any(queryStr, [id]);
+};
 
 const addNewShow = async (showObj) => {
-    // console.log(showObj);
+  // console.log(showObj);
 
-    const newShowQStr = `INSERT INTO shows (title, img_url,user_id,genre_id,id) 
+  const newShowQStr = `INSERT INTO shows (title, img_url,user_id,genre_id,id) 
                         VALUES($/title/,$/img_url/,$/user_id/,$/genre_id/,$/id/) 
                         ON CONFLICT (title) DO UPDATE SET title=EXCLUDED.title
                         RETURNING shows.id
-                        `
+                        `;
 
-    let showId = await db.one(newShowQStr, {
-        title: showObj.title,
-        img_url: showObj.img_url,
-        user_id: showObj.user_id,
-        genre_id: showObj.genre_id,
-        id: showObj.show_id
-    })
+  let showId = await db.one(newShowQStr, {
+    title: showObj.title,
+    img_url: showObj.img_url,
+    user_id: showObj.user_id,
+    genre_id: showObj.genre_id,
+    id: showObj.show_id,
+  });
 
-    await addNewShowWatcher({ user_id: showObj.user_id, show_id: showId.id })
+  await addNewShowWatcher({ user_id: showObj.user_id, show_id: showId.id });
 
-    return showId
-}
+  return showId;
+};
 
 const addNewShowWatcher = async (watcherObj) => {
-    const newShowWatcherQStr = `
+  const newShowWatcherQStr = `
                         INSERT INTO showWatchers (user_id,show_id) 
                         VALUES($/user_id/,$/show_id/) RETURNING *
-                        `
+                        `;
 
-    return await db.one(newShowWatcherQStr, {
-        user_id: watcherObj.user_id,
-        show_id: watcherObj.show_id
-    })
-}
+  return await db.one(newShowWatcherQStr, {
+    user_id: watcherObj.user_id,
+    show_id: watcherObj.show_id,
+  });
+};
 
-
-
-const getShowsByGenreId = async (genreId) => await db.any("SELECT * from shows WHERE genre_id = $1", [genreId])
+const getShowsByGenreId = async (genreId) =>
+  await db.any("SELECT * from shows WHERE genre_id = $1", [genreId]);
 
 const getShowsByUserId = async (userId) => {
-    const queryStr = `SELECT 
+  const queryStr = `SELECT 
                     showWatchers.user_id, shows.title, 
                     shows.img_url, shows.id, genre_name
                     FROM 
@@ -77,16 +74,16 @@ const getShowsByUserId = async (userId) => {
                     INNER JOIN shows ON shows.id = show_id
                     INNER JOIN genres ON genres.id = genre_id
                     WHERE showWatchers.user_id = $1
-                    `
+                    `;
 
-    return await db.any(queryStr, [userId])
-}
+  return await db.any(queryStr, [userId]);
+};
 
 module.exports = {
-    getAllShows,
-    getShowsById,
-    getShowsByGenreId,
-    getShowsByUserId,
-    addNewShow,
-    addNewShowWatcher
-}
+  getAllShows,
+  getShowsById,
+  getShowsByGenreId,
+  getShowsByUserId,
+  addNewShow,
+  addNewShowWatcher,
+};
